9874300c2128fd722730a2fa550ebdf08ea7ef49
Viktor <example@gmail.com>
2024-09-03 10:44:45 +0200

    with use createAsyncThunk right version

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -4,7 +4,7 @@ import {useHttp} from '../../hooks/http.hook';
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
-};
+}
 
 export const fetchHeroes = createAsyncThunk(
     'heroes/fetchHeroes',
@@ -18,9 +18,25 @@ const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
-        heroCreated: (state, action) => {state.heroes.push(action.payload)},
-        heroDeleted: (state, action) => {state.heroes = state.heroes.filter(item => item.id !== action.payload)}
+        heroCreated: (state, action) => {
+            state.heroes.push(action.payload);
+        },
+        heroDeleted: (state, action) => {
+            state.heroes = state.heroes.filter(item => item.id !== action.payload);
+        }
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = 'idle';
+                state.heroes = action.payload;
+            })
+            .addCase(fetchHeroes.rejected, state => {
+                state.heroesLoadingStatus = 'error';
+            })
+            .addDefaultCase(() => {})
+    }
 });
 
 const {actions, reducer} = heroesSlice;
