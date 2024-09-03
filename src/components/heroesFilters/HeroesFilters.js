8147421a3d1cfa5dab5ef444c81de82c101dae54
Viktor <example@gmail.com>
2024-09-03 10:25:34 +0200

    with use createAsyncThunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,21 +1,16 @@
-import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
-
-import { fetchFilters } from '../../actions';
-import { filtersChanged } from './filtersSlice';
+import { filtersChanged, fetchFilters } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
 
     const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
     const dispatch = useDispatch();
-    const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(fetchFilters(request));
-
+        dispatch(fetchFilters());
         // eslint-disable-next-line
     }, []);
 
--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,23 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {useHttp} from '../../hooks/http.hook';
 
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
 };
 
+export const fetchHeroes = createAsyncThunk(
+    'heroes/fetchHeroes',
+    async () => {
+        const {request} = useHttp();
+        return await request("http://localhost:3001/heroes");
+    }
+);
+
 const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
-        heroesFetching: state => {state.heroesLoadingStatus = 'loading'},
-        heroesFetched: (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = action.payload;
-        },
-        heroesFetchingError: state => {state.heroesLoadingStatus = 'error'},
         heroCreated: (state, action) => {state.heroes.push(action.payload)},
         heroDeleted: (state, action) => {state.heroes = state.heroes.filter(item => item.id !== action.payload)}
-    }
+    },
 });
 
 const {actions, reducer} = heroesSlice;
